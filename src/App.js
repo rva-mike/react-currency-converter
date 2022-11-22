a10856c1480a617738af74ce0464d9866dc830bb
rva-mike <mike.levy.rva@gmail.com>
2022-11-21 19:56:20 -0500

    basic styles, layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import CurrencyRow from "./components/CurrencyRow";
 
 function App() {
   return (
+    <>
     <h1>Convert</h1>
+    <CurrencyRow />
+    <div className="equals">=</div>
+    <CurrencyRow />
+    </>
   );
 }
 
---
+++ src/components/CurrencyRow.js
@@ -0,0 +1,10 @@
+export default function CurrencyRow(){
+    return (
+        <div>
+            <input type='number' className="input" />
+            <select>
+                <option value='Hi'>Hi</option>
+            </select>
+        </div>
+    )
+}
